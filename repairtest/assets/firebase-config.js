c1e2dae3c71ffaf0621fffc75f1f570d95c9c57a
allaouaboucheneb04-lgtm <247711883+allaouaboucheneb04-lgtm@users.noreply.github.com>
2026-03-05 12:49:08 -0500

    recoretions

---
+++ repairtest/assets/firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB0dGjtVj1_A35RPuRWYAZRpRbVwEE_Q5k",
+  authDomain: "garage-pro-one.firebaseapp.com",
+  projectId: "garage-pro-one",
+  storageBucket: "garage-pro-one.firebasestorage.app",
+  messagingSenderId: "873293567977",
+  appId: "1:873293567977:web:04fdbb9deb08e2bc3f9d78",
+  measurementId: "G-4J4XB4PS8E"
+};
