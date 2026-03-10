9cbca8bfb70cdb3f243cb956ce4c01b23884114e
Allaoua Boucheneb <allaouaboucheneb04@gmail.com>
2026-03-10 13:46:15 -0400

    icon

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,29 @@
-// Garage Pro One - simple offline cache
-const CACHE = "garagepro-v1";
-const CORE = [
-  "./",
-  "./index.html",
-  "./assets/style.css",
-  "./assets/app.js",
-  "./assets/firebase-config.js",
-  "./assets/logo-128.png",
-  "./assets/logo.png",
-  "./assets/favicon.ico",
-  "./assets/favicon-32.png",
-  "./assets/favicon-16.png",
-  "./assets/apple-touch-icon.png",
-  "./manifest.webmanifest"
+const CACHE_NAME = 'ab-garage-pro-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './register.html',
+  './superadmin.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png',
+  './assets/style.css',
+  './assets/app.js',
+  './assets/firebase-config.js',
+  './assets/logo.png'
 ];
-
-self.addEventListener("install", (e)=>{
-  e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())
-  );
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
-self.addEventListener("activate", (e)=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))).then(()=>self.clients.claim())
-  );
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
-self.addEventListener("fetch", (e)=>{
-  const req = e.request;
-  if(req.method !== "GET") return;
-  e.respondWith(
-    caches.match(req).then(hit=> hit || fetch(req).then(res=>{
-      const copy = res.clone();
-      caches.open(CACHE).then(c=>c.put(req, copy)).catch(()=>{});
-      return res;
-    }).catch(()=>hit))
-  );
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
+    const copy = resp.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
+    return resp;
+  }).catch(() => caches.match('./index.html'))));
 });
